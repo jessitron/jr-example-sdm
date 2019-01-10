f9f0fd958160b754a0e76c2db9bafff3d10d175a
Rod Johnson <rod@atomist.com>
2019-01-09 18:26:31 -0800

    Add formatUtils

---
+++ lib/format/formatUtils.ts
@@ -0,0 +1,115 @@
+import * as _ from "lodash";
+
+/**
+ * Formatting information at a particular point in a file
+ */
+export interface FormatPoint {
+    indentUnit: string;
+    depth: number;
+}
+
+export const DefaultIndentUnit = "   ";
+
+/**
+ * Find the format in the file at the given point
+ * @param {string} s
+ * @param {number} offset
+ * @return {FormatPoint}
+ */
+export function formatAt(s: string, offset: number): FormatPoint {
+    const lines = s.split("\n");
+    let indentUnit: string;
+    if (s.includes("\t")) {
+        indentUnit = "\t";
+    } else {
+        let spaceLine = lines.find(l => l.startsWith(" "));
+        if (spaceLine) {
+            indentUnit = "";
+            while (spaceLine.startsWith(" ")) {
+                indentUnit += " ";
+                spaceLine = spaceLine.slice(1);
+            }
+        } else {
+            indentUnit = DefaultIndentUnit;
+        }
+    }
+    let depth = 0;
+    let lastLine = _.last(lines);
+    while (lastLine.startsWith(indentUnit)) {
+        ++depth;
+        lastLine = lastLine.slice(indentUnit.length);
+    }
+    return {
+        indentUnit,
+        depth,
+    };
+}
+
+/**
+ * FormatPoint at the end of the given string
+ * @param {string} s
+ * @return {FormatPoint}
+ */
+export function formatAtEndOf(s: string): FormatPoint {
+    return formatAt(s, s.length - 1);
+}
+
+/**
+ * Insert the given formatted string into the original string at the given offset
+ * @param {string} s string to insert into
+ * @param {number} offset offset within the string to insert at
+ * @param {string} what formatted string to insert. The string should use tabs
+ * in place of indentation units. It does not need to worry about any existing level of
+ * indentation, as this will be computed automatically.
+ * The tab will be converted to whatever indentation unit the input string
+ * uses.
+ * @return {string}
+ */
+export function insertFormatted(s: string, offset: number, what: string): string {
+    const fp = formatAt(s, offset);
+    return insertAt(s, offset, indent(what, fp));
+}
+
+/**
+ * Append to given string, detecting and honoring its formatting
+ * @param left string to append to
+ * @param what to append
+ * @return {string}
+ */
+export function appendFormatted(left: string, what: string): string {
+    const fp = formatAtEndOf(left);
+    return left + indent(what, fp);
+}
+
+/**
+ * Indent the given content appropriately. it should have its
+ * own internal indentation
+ * @return {string}
+ */
+export function indent(what: string, formatPoint: FormatPoint): string {
+    return what
+        .replace("\t", formatPoint.indentUnit)
+        .split("\n")
+        .map(line => {
+            if (line === "") {
+                // Don't pad an empty line
+                return line;
+            }
+            let pad = "";
+            for (let i = 0; i < formatPoint.depth; i++) {
+                pad += formatPoint.indentUnit;
+            }
+            return pad + line;
+        })
+        .join("\n");
+}
+
+/**
+ * Insert at the given position
+ * @param {number} position
+ * @param {string} what
+ * @return {Promise<void>}
+ */
+export function insertAt(oldContent: string, position: number, what: string): string {
+    return oldContent.substr(0, position) + what + oldContent.substr(position);
+}
---
+++ test/format/formatUtils.test.ts
@@ -0,0 +1,74 @@
+import { appendFormatted, DefaultIndentUnit, formatAtEndOf, insertFormatted } from "../../lib/format/formatUtils";
+import * as assert from "assert";
+
+describe("formatUtils", () => {
+
+    describe("formatPoint", () => {
+
+        it("should work with empty string", () => {
+            const input = "";
+            const fp = formatAtEndOf(input);
+            assert.deepStrictEqual(fp, { depth: 0, indentUnit: DefaultIndentUnit });
+        });
+    });
+
+    describe("insertFormatted", () => {
+
+        it("should insert in empty string", () => {
+            const input = "";
+            const r = insertFormatted(input, 0, "a=b");
+            assert.strictEqual(r, "a=b");
+        });
+
+    });
+
+    describe("appendFormatted", () => {
+
+        it("should insert after empty string", () => {
+            const left = "";
+            const r = appendFormatted(left, "a=b");
+            assert.strictEqual(r, "a=b");
+        });
+
+        it("should insert after non-empty string", () => {
+            const left = "left";
+            const r = appendFormatted(left, "a=b");
+            assert.strictEqual(r, "lefta=b");
+        });
+
+        describe("tab support", () => {
+
+            it("should append indented", () => {
+                const left = "public class Foo\n\tint i = 0;";
+                const r = appendFormatted(left, "\nint j = 1;\n");
+                assert.strictEqual(r, left + "\n\tint j = 1;\n");
+            });
+
+            it("should append double indented", () => {
+                const left = "public class Foo\n\tint i = 0;";
+                const r = appendFormatted(left, "\nint j = 1;\n\tx\n");
+                assert.strictEqual(r, left + "\n\tint j = 1;\n\t\tx\n");
+            });
+
+        });
+
+        describe("space... support", () => {
+            const indent = "   ";
+
+            it("should append indented", () => {
+                const left = `public class Foo\n${indent}int i = 0;`;
+                const r = appendFormatted(left, "\nint j = 1;\n");
+                assert.strictEqual(r, left + `\n${indent}int j = 1;\n`);
+            });
+
+            it("should insert double indented", () => {
+                const left = `public class Foo\n${indent}int i = 0;`;
+                const r = appendFormatted(left, "\nint j = 1;\n\tx\n");
+                assert.strictEqual(r, left + `\n${indent}int j = 1;\n${indent}${indent}x\n`);
+            });
+
+        });
+
+    });
+
+});
